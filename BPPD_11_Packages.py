095742ccfb844fa233f57564e30472de9b2504e3
ammrlc <an6elam@msn.com>
2019-08-13 14:30:21 -0400

    new chapter exercises

---
+++ BPPD_11_Packages.py
@@ -0,0 +1,19 @@
+
+# coding: utf-8
+
+# # Creating Code Groupings
+
+# In[1]:
+
+def SayHello(Name):
+   print("Hello ", Name)
+   return
+def SayGoodbye(Name):
+   print("Goodbye ", Name)
+   return
+
+
+# In[ ]:
+
+
+
